99df17448a448228e00340624632fcb97d60450c
enes-tavukcu <enes.tavukcu@edu.univ-fcomte.fr>
2024-11-26 13:42:18 +0100

    feat(test) : :rocket: test many

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-
+import { RouterLink, RouterView } from "vue-router";
+import Button from "./components/Button.vue";
 </script>
 
 <template>
   <RouterView />
 </template>
-
