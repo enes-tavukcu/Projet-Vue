5e0e5f933f2cab55b93626c080734278c5667022
enes-tavukcu <enes.tavukcu@edu.univ-fcomte.fr>
2024-12-09 15:50:25 +0100

    feat(index) : ajout banner hom + block acutalite

---
+++ components/CardNews.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+defineProps<{
+  image: string;
+  titre: string;
+  text: string;
+}>();
+
+// === Gestion des Images ===
+const { urlFor } = useSanityImage();
+</script>
+
+<template>
+  <div class="latest-news">
+    <img v-if="image" :src="urlFor(image)?.url()" alt="Image du personnage">
+
+    <p class="latest-news__item-title">
+      {{ titre }}
+    </p>
+  </div>
+  <p class="latest-news__item-text">{{ text }}</p>
+</template>
+
+<style lang="scss">
+.latest-news {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+
+  &__item-text {
+    font-size: 10px;
+    color: #666;
+    font-family: $fontTitleFamily;
+    text-align: left;
+  }
+  &__item-title {
+    font-size: 14px;
+    font-family: $fontTitleFamily;
+    text-align: center;
+    font-weight: 600;
+  }
+  }
+
+ 
+
+</style>
